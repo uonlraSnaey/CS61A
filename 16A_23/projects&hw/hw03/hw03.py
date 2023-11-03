1eaffa2dc7fe4a1e65264cacb853e008efae82e7
Ciencat <pinchu_uonlrasanye@163.com>
2023-11-03 16:37:42 +0800

    update lecture7_hw03

--- 16A_23/projects&hw/hw03/hw03.py
+++ 16A_23/projects&hw/hw03/hw03.py
@@ -21,6 +21,9 @@ def composer(func=lambda x: x):
     """
     def func_adder(g):
         "*** YOUR CODE HERE ***"
+        f = lambda x: func(g(x))
+        return composer(f)
+
     return func, func_adder
 
 
@@ -43,6 +46,10 @@ def g(n):
     True
     """
     "*** YOUR CODE HERE ***"
+    if n <= 3:
+        return n
+    else:
+        return g(n-1) + 2*g(n-2) + 3*g(n-3)
 
 def g_iter(n):
     """Return the value of G(n), computed iteratively.
@@ -63,6 +70,17 @@ def g_iter(n):
     True
     """
     "*** YOUR CODE HERE ***"
+    if n <= 3:
+        return n
+    if n > 3:
+        g1, g2, g3 = 3, 2, 1
+        for i in range(4, n+1):
+            current_g = g1 + 2*g2 + 3*g3
+            g3, g2, g1 = g2, g1, current_g
+
+    return g1
+
+
 
 
 def missing_digits(n):
@@ -93,7 +111,15 @@ def missing_digits(n):
     True
     """
     "*** YOUR CODE HERE ***"
+    # 环境图
+    # https://pythontutor.com/cp/composingprograms.html#code=def%20missing_digits%28n%29%3A%0A%20%20%20%20if%20n%20%3C%2010%3A%0A%20%20%20%20%20%20%20%20return%200%0A%20%20%20%20else%3A%0A%20%20%20%20%20%20%20%20last_digit%20%3D%20n%20%25%2010%0A%20%20%20%20%20%20%20%20second_last_digit%20%3D%20%28n%20//%2010%29%20%25%2010%0A%20%20%20%20%20%20%20%20return%20%28last_digit%20-%20second_last_digit%20-%201%29%20%2B%20missing_digits%28n%20//%2010%29%0A%0As%20%3D%20missing_digits%28157%29%0Aprint%28s%29&cumulative=true&curInstr=16&mode=display&origin=composingprograms.js&py=3&rawInputLstJSON=%5B%5D
+    if n < 10:
+        return 0
+    else:
+        last_digit = n % 10
+        before_last_digit = (n // 10) % 10
 
+        return max(0, last_digit - before_last_digit - 1) + missing_digits(n // 10)
 
 def count_change(total):
     """Return the number of ways to make change for total.
@@ -112,7 +138,42 @@ def count_change(total):
     True
     """
     "*** YOUR CODE HERE ***"
+    def make_change(amount, coin):
+        if amount == 0:
+            return 1
+        if amount < 0 or coin == 0:
+            return 0
+        else:
+            without_coin = make_change(amount, coin//2)  # 不用当前硬币
+            with_coin = make_change(amount-coin, coin)  # 用
+            return with_coin + without_coin
+    '''
+    biggest_coin = 1  # 老实从小到大找
+    while biggest_coin * 2 <= total:
+        biggest_coin = biggest_coin * 2  # 硬币的面值是2的幂次方
 
+    return make_change(total, biggest_coin)
+    '''
+    def find_larger_coin(coin):
+        if coin * 2 <= total:
+            return find_larger_coin(coin*2)
+        else:
+            return coin
+
+    larger_coin = find_larger_coin(1)
+    return make_change(total, larger_coin)
+
+    '''
+        # n,m: 找零方式，当前coin面值
+        def helper(n, m):
+        if n == 0:
+            return 1
+        elif n < (1 << m):
+            return 0
+        else:
+            return helper(n, m + 1) + helper(n - (1 << m), m)  # 2的m次幂
+    return helper(total, 0)
+    '''
 
 def print_move(origin, destination):
     """Print instructions to move a disk."""
@@ -147,7 +208,13 @@ def move_stack(n, start, end):
     """
     assert 1 <= start <= 3 and 1 <= end <= 3 and start != end, "Bad start/end"
     "*** YOUR CODE HERE ***"
-
+    if n == 1:
+        print_move(start, end)
+    else:
+        middle = 6 - start - end
+        move_stack(n-1, start, middle)
+        print_move(start, end)
+        move_stack(n-1, middle, end)
 
 from operator import sub, mul
 
@@ -161,5 +228,5 @@ def make_anonymous_factorial():
     >>> check(HW_SOURCE_FILE, 'make_anonymous_factorial', ['Assign', 'AugAssign', 'FunctionDef', 'Recursion'])
     True
     """
-    return 'YOUR_EXPRESSION_HERE'
+    return (lambda f: lambda k: f(f, k))(lambda f, k: k if k == 1 else mul(k, f(f, sub(k, 1))))
 
